d5eb3f5af9d380e0fbc66dacbb22636fb61692d2
elanez <61147852+elanez@users.noreply.github.com>
2022-11-23 12:39:30 +0800

    Update force mode

--- Assets/Scripts/Holen/InteractableHolen.cs
+++ Assets/Scripts/Holen/InteractableHolen.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class InteractableHolen : MonoBehaviour
 {
-    Rigidbody rb;
+    private Rigidbody _rb;
+    public float power = 100f;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate() 
+    {
+        if(Input.GetMouseButtonDown(0))
+            MoveHolen(power);
     }
 
     public void MoveHolen(float power)
     {
-        rb.AddForce(transform.forward * power);
+        _rb.AddForce(transform.forward * power, ForceMode.Impulse);
     }
 }
